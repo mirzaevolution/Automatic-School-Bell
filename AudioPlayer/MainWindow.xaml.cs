a9eb36304fe36713c87d790c3fea284cf85939fb
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-11-29 10:29:35 +0700

    Adding automatic shutdown when audio ended for AudioPlayer

--- AudioPlayer/MainWindow.xaml.cs
+++ AudioPlayer/MainWindow.xaml.cs
@@ -8,18 +8,30 @@ namespace AudioPlayer
 {
     public partial class MainWindow : Window
     {
-        private MediaPlayer player = new MediaPlayer()
-        {
-            Volume = 1.0
-        };
+        private MediaPlayer player;
         public MainWindow()
         {
             InitializeComponent();
-
+            player = new MediaPlayer
+            {
+                Volume = 1.0f
+            };
+            player.MediaEnded += Player_MediaEnded;
         }
+
+        private void Player_MediaEnded(object sender, EventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
         public MainWindow(string audioLocation)
         {
             InitializeComponent();
+            player = new MediaPlayer
+            {
+                Volume = 1.0f
+            };
+            player.MediaEnded += Player_MediaEnded;
             LoadAudio(audioLocation);
         }
         private void LoadAudio(string audioLocation)
