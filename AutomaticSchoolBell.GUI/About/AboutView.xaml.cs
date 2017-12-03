8d35dccee54c79627cbb48e46b3d05a684fd5317
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-12-03 14:41:13 +0700

    Adding about page. Adding app.manifest. Fixing bugs

---
+++ AutomaticSchoolBell.GUI/About/AboutView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MahApps.Metro.Controls;
+using System.IO;
+namespace AutomaticSchoolBell.GUI.About
+{
+    /// <summary>
+    /// Interaction logic for AboutView.xaml
+    /// </summary>
+    public partial class AboutView : MetroWindow
+    {
+        public AboutView()
+        {
+            InitializeComponent();
+            Load();
+        }
+        private async void Load()
+        {
+            if(File.Exists("apache-license.txt"))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader("apache-license.txt"))
+                    {
+                        string text = await reader.ReadToEndAsync();
+                        TextBoxLicense.Text = text;
+                    }
+                }
+                catch { }
+            }
+        }
+        private void GotoUri(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch { }
+        }
+    }
+}
--- AutomaticSchoolBell.GUI/MainWindow.xaml.cs
+++ AutomaticSchoolBell.GUI/MainWindow.xaml.cs
@@ -1,22 +1,9 @@
 ﻿using MahApps.Metro.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using AutomaticSchoolBell.GUI.About;
 namespace AutomaticSchoolBell.GUI
 {
-   
+
     public partial class MainWindow : MetroWindow
     {
        
@@ -34,7 +21,7 @@ namespace AutomaticSchoolBell.GUI
 
         private void ButtonAbout_Click(object sender, RoutedEventArgs e)
         {
-
+            new AboutView().ShowDialog();
         }
     }
 }
